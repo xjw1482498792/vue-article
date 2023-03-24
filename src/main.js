8706922603e463689559feb15af6e9cf64b2c9c0
xjw <1482498792@qq.com>
2023-03-24 22:22:08 +0800

    second

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/scss/index.scss';
 
 Vue.config.productionTip = false;
 
